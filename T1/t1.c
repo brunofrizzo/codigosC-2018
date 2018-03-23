05ca273e980f363af5e479bfb58f04ebb2805167
Unknown <brunofrizzo@outlook.com>
2018-03-22 22:44:29 -0300

    Posicionar navios aleatoriamente funcionando.

--- T1/t1.c
+++ T1/t1.c
@@ -39,7 +39,6 @@ void imprime_matriz(int **m, int l, int c) {
 }
 
 int num_rand(int limite) {
-	srand(time(NULL));
 	return (rand() % limite) + 1;
 }
 
@@ -72,6 +71,310 @@ int verifica_validade_navios(int v[], int l, int c) {
 
 }
 
+int validade_direcao_navio1(int direcao, int l, int c, int **m, int linhas, int colunas) {
+	int validade=0;
+
+	if(m[l][c] == 0) {
+		if (direcao == 1){
+			if(l-1 < 1){
+				validade = 0;
+			} else {
+				if (m[l-1][c] == 0) {
+					validade = 1;
+				}else{
+					validade = 0;
+				}
+			}
+		}else if (direcao == 2) {
+			if (c-1 < 1) {
+				validade = 0;
+			}else{
+				if (m[l][c-1] == 0) {
+					validade = 1;
+				}else{
+					validade = 0;
+				}
+			}
+		} else if (direcao == 3) {
+			if (l+1 > linhas) {
+				validade = 0;
+			}else{
+				if (m[l+1][c] == 0) {
+					validade = 1;
+				}else{
+					validade = 0;
+				}
+			}
+		} else {
+			if (c+1 > colunas) {
+				validade = 0;
+			}else {
+				if (m[l][c+1] == 0) {
+					validade = 1;
+				}else{
+					validade = 0;
+				}
+			}
+		}
+	} else {
+		validade = 0;
+	}
+	
+	return validade;
+}
+
+int validade_direcao_navio2(int direcao, int l, int c, int **m, int linhas, int colunas) {
+	int validade=0;
+
+	if(m[l][c] == 0) {
+		if (direcao == 1){
+			if (l-2 < 1) {
+				validade = 0;
+			} else {
+				if (m[l-1][c] == 0) {
+					if (m[l-2][c] == 0) {
+						validade = 1;
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else if (direcao == 2) {
+			if (c-2 < 1) {
+				validade = 0;
+			}else{
+				if (m[l][c-1] == 0) {
+					if (m[l][c-2] == 0) {
+						validade = 1;
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else if (direcao == 3) {
+			if (l+2 > linhas) {
+				validade = 0;
+			}else{
+				if (m[l+1][c] == 0) {
+					if (m[l+2][c] == 0) {
+						validade = 1;
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else {
+			if (c+2 > colunas) {
+				validade = 0;
+			}else{
+				if (m[l][c+1] == 0) {
+					if (m[l][c+2] == 0) {
+						validade = 1;
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} 
+	} else{
+		validade = 0;
+	}
+
+	return validade;
+}
+
+int validade_direcao_navio3(int direcao, int l, int c, int **m, int linhas, int colunas) {
+	int validade=0;
+
+	if (m[l][c] == 0) {
+		if (direcao == 1){
+			if (l-3 < 1) {
+				validade = 0;
+			}else{
+				if (m[l-1][c] == 0) {
+					if (m[l-2][c] == 0) {
+						if (m[l-3][c] == 0) {
+							validade = 1;
+						} else {
+							validade = 0;
+						}
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else if (direcao == 2) {
+			if (c-3 < 1) {
+				validade = 0;
+			}else{
+				if (m[l][c-1] == 0) {
+					if (m[l][c-2] == 0) {
+						if (m[l][c-3] == 0) {
+							validade = 1;
+						} else {
+							validade = 0;
+						}
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else if (direcao == 3) {
+			if (l+3 > linhas) {
+				validade = 0;
+			}else{
+				if (m[l+1][c] == 0) {
+					if (m[l+2][c] == 0) {
+						if (m[l+3][c] == 0) {
+							validade = 1;
+						} else {
+							validade = 0;
+						}
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else {
+			if (c+3 > colunas) {
+				validade = 0;
+			} else{
+				if (m[l][c+1] == 0) {
+					if (m[l][c+2] == 0) {
+						if (m[l][c+3] == 0) {
+							validade = 1;
+						} else {
+							validade = 0;
+						}
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}	
+			}
+		}
+	}else{
+		validade = 0;
+	}
+	
+	return validade;
+}
+
+int validade_direcao_navio4(int direcao, int l, int c, int **m, int linhas, int colunas) {
+	int validade=0;
+
+	if (m[l][c] == 0) {
+		if (direcao == 1){
+			if (l-4 < 1) {
+				validade = 0;
+			}else{
+				if (m[l-1][c] == 0) {
+					if (m[l-2][c] == 0) {
+						if (m[l-3][c] == 0) {
+							if (m[l-4][c] == 0) {
+								validade = 1;
+							}else{
+								validade = 0;
+							}
+						} else {
+							validade = 0;
+						}
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else if (direcao == 2) {
+			if (c-4 < 1) {
+				validade = 0;
+			}else{
+				if (m[l][c-1] == 0) {
+					if (m[l][c-2] == 0) {
+						if (m[l][c-3] == 0) {
+							if (m[l][c-4] == 0) {
+								validade = 1;
+							}else{
+								validade = 0;
+							}
+						} else {
+							validade = 0;
+						}
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else if (direcao == 3) {
+			if (l+4 > linhas) {
+				validade = 0;
+			}else{
+				if (m[l+1][c] == 0) {
+					if (m[l+2][c] == 0) {
+						if (m[l+3][c] == 0) {
+							if (m[l+4][c] == 0) {
+								validade = 1;
+							}else{
+								validade = 0;
+							}
+						} else {
+							validade = 0;
+						}
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		} else {
+			if (c+4 > colunas) {
+				validade = 0;
+			}else{
+				if (m[l][c+1] == 0) {
+					if (m[l][c+2] == 0) {
+						if (m[l][c+3] == 0) {
+							if (m[l][c+4] == 0) {
+								validade = 1;
+							}else{
+								validade = 0;
+							}
+						} else {
+							validade = 0;
+						}
+					}else{
+						validade = 0;
+					}
+				}else{
+					validade = 0;
+				}
+			}
+		}
+	}else{
+		validade = 0;
+	}
+	
+	return validade;
+}
+
 void insere_navio(int tipo_navio, int linhas, int colunas, int **m) {
 
 	int i, l, c, direcao, validade = 0;
@@ -79,379 +382,139 @@ void insere_navio(int tipo_navio, int linhas, int colunas, int **m) {
 	switch (tipo_navio) {
 
 		case 1:
-			l = num_rand(linhas);
-			c = num_rand(colunas);
+			l = num_rand(linhas)-1;
+			c = num_rand(colunas)-1;
+			direcao = num_rand(4);
 
 			//Verifica se ponto aleatorio nao esta ocupado por um navio
-			while(m[l][c]==1) { 
-				l = num_rand(linhas);
-				c = num_rand(colunas);
+			validade = validade_direcao_navio1(direcao, l, c, m, linhas-1, colunas-1);
+			while(validade == 0) { 
+				l = num_rand(linhas)-1;
+				c = num_rand(colunas)-1;
+				direcao = num_rand(4);
+				validade = validade_direcao_navio1(direcao, l, c, m, linhas-1, colunas-1);
 			}
 
-			//Escolhe uma direcao aleatoria e verifica se todos os pontos naquela direcao estao livres
-			while (validade != 1) {
-				direcao = num_rand(4);
+			printf("Navio 1. Ponto: %d - %d. Direcao: %d. Validade = %d\n", l+1, c+1, direcao, validade);
 
-				if (direcao == 1){
-					if (l-1 < 1) {
-						validade = 0;
-					}else{
-						if (m[l-1][c] == 0) {
-							m[l-1][c] = 1;
-							validade = 1;
-						}else{
-							validade = 0;
-						}
-					}
-				} else if (direcao == 2) {
-					if (c-1 < 1) {
-						validade = 0;
-					}else{
-						if (m[l][c-1] == 0) {
-							m[l][c-1] = 1;
-							validade = 1;
-						}else{
-							validade = 0;
-						}
-					}
-				} else if (direcao == 3) {
-					if (l+1 > linhas) {
-						validade = 0;
-					}else{
-						if (m[l+1][c] == 0) {
-							m[l+1][c] = 1;
-							validade = 1;
-						}else{
-							validade = 0;
-						}
-					}
-				} else {
-					if (c+1 > colunas) {
-						validade = 0;
-					}else {
-						if (m[l][c+1] == 0) {
-							m[l][c+1] = 1;
-							validade = 1;
-						}else{
-							validade = 0;
-						}
-					}
-				}
-
-			} //end while
+			if (direcao == 1) {
+				m[l-1][c] = 1;
+			}else if (direcao == 2) {
+				m[l][c-1] = 1;
+			} else if (direcao == 3) {
+				m[l+1][c] = 1;
+			}else{
+				m[l][c+1] = 1;
+			}
 			m[l][c] = 1;
 		break;
 
 		case 2:
-			l = num_rand(linhas);
-			c = num_rand(colunas);
+			l = num_rand(linhas)-1;
+			c = num_rand(colunas)-1;
+			direcao = num_rand(4);
 
 			//Verifica se ponto aleatorio nao esta ocupado por um navio
-			while(m[l][c]==1) { 
-				l = num_rand(linhas);
-				c = num_rand(colunas);
-			}
-
-			//Escolhe uma direcao aleatoria e verifica se todos os pontos naquela direcao estao livres
-			while(validade != 1) {
+			validade = validade_direcao_navio2(direcao, l, c, m, linhas-1, colunas-1);
+			while(validade == 0) { 
+				l = num_rand(linhas)-1;
+				c = num_rand(colunas)-1;
 				direcao = num_rand(4);
+				validade = validade_direcao_navio2(direcao, l, c, m, linhas-1, colunas-1);
+			} 
 
-				
-				if (direcao == 1){
-					if (l-2 < 1) {
-						validade = 0;
-					} else {
-						if (m[l-1][c] == 0) {
-							m[l-1][c] = 1;
-							if (m[l-2][c] == 0) {
-								m[l-2][c] = 1;
-								validade = 1;
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else if (direcao == 2) {
-					if (c-2 < 1) {
-						validade = 0;
-					}else{
-						if (m[l][c-1] == 0) {
-							m[l][c-1] = 1;
-							if (m[l][c-2] == 0) {
-								m[l][c-2] = 1;
-								validade = 1;
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else if (direcao == 3) {
-					if (l+2 > linhas) {
-						validade = 0;
-					}else{
-						if (m[l+1][c] == 0) {
-							m[l+1][c] = 1;
-							if (m[l+2][c] == 0) {
-								m[l+2][c] = 1;
-								validade = 1;
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else {
-					if (c+2 > colunas) {
-						validade = 0;
-					}else{
-						if (m[l][c+1] == 0) {
-							m[l][c+1] = 1;
-							if (m[l][c+2] == 0) {
-								m[l][c+2] = 1;
-								validade = 1;
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				}
+			printf("Navio 2. Ponto: %d - %d. Direcao: %d. Validade = %d\n", l+1, c+1, direcao, validade);
 
-			} //end while
+			if (direcao == 1) {
+				m[l-1][c] = 1;
+				m[l-2][c] = 1;
+			}else if (direcao == 2) {
+				m[l][c-1] = 1;
+				m[l][c-2] = 1;
+			} else if (direcao == 3) {
+				m[l+1][c] = 1;
+				m[l+2][c] = 1;
+			}else{
+				m[l][c+1] = 1;
+				m[l][c+2] = 1;
+			}
 			m[l][c] = 1;
 		break;
 
 		case 3:
-			l = num_rand(linhas);
-			c = num_rand(colunas);
+			l = num_rand(linhas)-1;
+			c = num_rand(colunas)-1;
+			direcao = num_rand(4);
 
 			//Verifica se ponto aleatorio nao esta ocupado por um navio
-			while(m[l][c]==1) { 
-				l = num_rand(linhas);
-				c = num_rand(colunas);
+			validade = validade_direcao_navio3(direcao, l, c, m, linhas-1, colunas-1);
+			while(validade == 0) { 
+				l = num_rand(linhas)-1;
+				c = num_rand(colunas)-1;
+				direcao = num_rand(4);
+				validade = validade_direcao_navio3(direcao, l, c, m, linhas-1, colunas-1);
 			}
 
-			//Escolhe uma direcao aleatoria e verifica se todos os pontos naquela direcao estao livres
-			while(validade != 1) {
-				direcao = num_rand(4);
+			printf("Navio 3. Ponto: %d - %d. Direcao: %d. Validade = %d\n", l+1, c+1, direcao, validade);
 
-				if (direcao == 1){
-					if (l-3 < 1) {
-						validade = 0;
-					}else{
-						if (m[l-1][c] == 0) {
-							m[l-1][c] = 1;
-							if (m[l-2][c] == 0) {
-								m[l-2][c] = 1;
-								if (m[l-3][c] == 0) {
-									m[l-3][c] = 1;
-									validade = 1;
-								} else {
-									validade = 0;
-								}
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else if (direcao == 2) {
-					if (c-3 < 1) {
-						validade = 0;
-					}else{
-						if (m[l][c-1] == 0) {
-							m[l][c-1] = 1;
-							if (m[l][c-2] == 0) {
-								m[l][c-2] = 1;
-								if (m[l][c-3] == 0) {
-									m[l][c-3] = 1;
-									validade = 1;
-								} else {
-									validade = 0;
-								}
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else if (direcao == 3) {
-					if (l+3 > linhas) {
-						validade = 0;
-					}else{
-						if (m[l+1][c] == 0) {
-							m[l+1][c] = 1;
-							if (m[l+2][c] == 0) {
-								m[l+2][c] = 1;
-								if (m[l+3][c] == 0) {
-									m[l+3][c] = 1;
-									validade = 1;
-								} else {
-									validade = 0;
-								}
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else {
-					if (c+3 > colunas) {
-						validade = 0;
-					} else{
-						if (m[l][c+1] == 0) {
-							m[l][c+1] = 1;
-							if (m[l][c+2] == 0) {
-								m[l][c+2] = 1;
-								if (m[l][c+3] == 0) {
-									m[l][c+3] = 1;
-									validade = 1;
-								} else {
-									validade = 0;
-								}
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}	
-					}
-				}
 
-			} //while
+			if (direcao == 1) {
+				m[l-1][c] = 1;
+				m[l-2][c] = 1;
+				m[l-3][c] = 1;
+			}else if (direcao == 2) {
+				m[l][c-1] = 1;
+				m[l][c-2] = 1;
+				m[l][c-3] = 1;
+			} else if (direcao == 3) {
+				m[l+1][c] = 1;
+				m[l+2][c] = 1;
+				m[l+3][c] = 1;
+			}else{
+				m[l][c+1] = 1;
+				m[l][c+2] = 1;
+				m[l][c+3] = 1;
+			}
 			m[l][c] = 1;
 		break;
 
 		case 4:
-			l = num_rand(linhas);
-			c = num_rand(colunas);
+			l = num_rand(linhas)-1;
+			c = num_rand(colunas)-1;
+			direcao = num_rand(4);
 
 			//Verifica se ponto aleatorio nao esta ocupado por um navio
-			while(m[l][c]==1) { 
-				l = num_rand(linhas);
-				c = num_rand(colunas);
+			validade = validade_direcao_navio4(direcao, l, c, m, linhas-1, colunas-1);
+			while(validade == 0) { 
+				l = num_rand(linhas)-1;
+				c = num_rand(colunas)-1;
+				direcao = num_rand(4);
+				validade = validade_direcao_navio4(direcao, l, c, m, linhas-1, colunas-1);
 			}
 
-			//Escolhe uma direcao aleatoria e verifica se todos os pontos naquela direcao estao livres
-			while(validade != 1) {
-				direcao = num_rand(4);
+			printf("Navio 4. Ponto: %d - %d. Direcao: %d. Validade = %d\n", l+1, c+1, direcao, validade);
 
-				if (direcao == 1){
-					if (l-4 < 1) {
-						validade = 0;
-					}else{
-						if (m[l-1][c] == 0) {
-							m[l-1][c] = 1;
-							if (m[l-2][c] == 0) {
-								m[l-2][c] = 1;
-								if (m[l-3][c] == 0) {
-									m[l-3][c] = 1;
-									if (m[l-4][c] == 0) {
-										m[l-4][c] = 1;
-										validade = 1;
-									}else{
-										validade = 0;
-									}
-								} else {
-									validade = 0;
-								}
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else if (direcao == 2) {
-					if (c-4 < 1) {
-						validade = 0;
-					}else{
-						if (m[l][c-1] == 0) {
-							m[l][c-1] = 1;
-							if (m[l][c-2] == 0) {
-								m[l][c-2] = 1;
-								if (m[l][c-3] == 0) {
-									m[l][c-3] = 1;
-									if (m[l][c-4] == 0) {
-										m[l][c-4] = 1;
-										validade = 1;
-									}else{
-										validade = 0;
-									}
-								} else {
-									validade = 0;
-								}
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else if (direcao == 3) {
-					if (l+4 > linhas) {
-						validade = 0;
-					}else{
-						if (m[l+1][c] == 0) {
-							m[l+1][c] = 1;
-							if (m[l+2][c] == 0) {
-								m[l+2][c] = 1;
-								if (m[l+3][c] == 0) {
-									m[l+3][c] = 1;
-									if (m[l+4][c] == 0) {
-										m[l+4][c] = 1;
-										validade = 1;
-									}else{
-										validade = 0;
-									}
-								} else {
-									validade = 0;
-								}
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				} else {
-					if (c+4 > colunas) {
-						validade = 0;
-					}else{
-						if (m[l][c+1] == 0) {
-							m[l][c+1] = 1;
-							if (m[l][c+2] == 0) {
-								m[l][c+2] = 1;
-								if (m[l][c+3] == 0) {
-									m[l][c+3] = 1;
-									if (m[l][c+4] == 0) {
-										m[l][c+4] = 1;
-										validade = 1;
-									}else{
-										validade = 0;
-									}
-								} else {
-									validade = 0;
-								}
-							}else{
-								validade = 0;
-							}
-						}else{
-							validade = 0;
-						}
-					}
-				}
-
-			} //while
+			if (direcao == 1) {
+				m[l-1][c] = 1;
+				m[l-2][c] = 1;
+				m[l-3][c] = 1;
+				m[l-4][c] = 1;
+			}else if (direcao == 2) {
+				m[l][c-1] = 1;
+				m[l][c-2] = 1;
+				m[l][c-3] = 1;
+				m[l][c-4] = 1;
+			} else if (direcao == 3) {
+				m[l+1][c] = 1;
+				m[l+2][c] = 1;
+				m[l+3][c] = 1;
+				m[l+4][c] = 1;
+			}else{
+				m[l][c+1] = 1;
+				m[l][c+2] = 1;
+				m[l][c+3] = 1;
+				m[l][c+4] = 1;
+			}
 			m[l][c] = 1;
 		break;
 
@@ -463,30 +526,47 @@ void insere_navio(int tipo_navio, int linhas, int colunas, int **m) {
 
 int main () {
 	
-	int opcao, linhas_m1, colunas_m1, i, j, vet_navios[4];
+	int opcao, linhas_m1, colunas_m1, i, vet_navios[4];
 
 	printf("Digite 1 se deseja jogar sozinho ou 2 se deseja jogar com o computador: \n");
 	scanf("%d", &opcao);
 
 	if (opcao == 1) {
 
-		le_dimensao(&linhas_m1, &colunas_m1);
+	srand(time(NULL));
+	le_dimensao(&linhas_m1, &colunas_m1);
+	// printf("n linhas = %d\n", linhas_m1);
+	// printf("n colunas = %d\n", colunas_m1);
+	// printf("numero l gerado = %d\n", num_rand(linhas_m1));
+	// printf("numero c gerado = %d\n", num_rand(colunas_m1));
+
 
 		int **matriz = cria_matriz(linhas_m1, colunas_m1);
 
 		// imprime_matriz(matriz, linhas_m1, colunas_m1);
 
-		// le_quant_navios(vet_navios);
+		le_quant_navios(vet_navios);
 
-		// if (verifica_validade_navios(vet_navios, linhas_m1, colunas_m1) == 1) {
+		if (verifica_validade_navios(vet_navios, linhas_m1, colunas_m1) == 1) {
 
-		// printf("-------------\n");
-			insere_navio(4, linhas_m1, colunas_m1, matriz);
+			for (i=0; i<vet_navios[0]; i++){
+				insere_navio(1, linhas_m1, colunas_m1, matriz);
+			}
+			for (i=0; i<vet_navios[1]; i++){
+				insere_navio(2, linhas_m1, colunas_m1, matriz);
+			}
+			for (i=0; i<vet_navios[2]; i++){
+				insere_navio(3, linhas_m1, colunas_m1, matriz);
+			}
+			for (i=0; i<vet_navios[3]; i++){
+				insere_navio(4, linhas_m1, colunas_m1, matriz);
+			}
+
 			imprime_matriz(matriz, linhas_m1, colunas_m1);
 
-		// } else {
-			// printf("Numero de navios informados inválido.\n");
-		// }
+		} else {
+			printf("Numero de navios informados inválido.\n");
+		}
 
 	} else if (opcao == 0) {
 
